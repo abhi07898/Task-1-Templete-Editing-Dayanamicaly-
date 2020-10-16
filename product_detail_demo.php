1605e982238ed60677a1848c3614d198532708ba
abhi07898 <abhi07898@gmail.com>
2020-10-16 18:22:30 +0530

    commit when i tried to use add to cart option

--- product_detail_demo.php
+++ product_detail_demo.php
@@ -2,11 +2,31 @@
 include 'admin/config.php';
 $sr_no=$_GET['sr_no'];
 
-$sql  = "SELECT  `NAME` , `PRICE`, `IMAGE`,  `DISCRIPTION`, `CATEGORY` FROM products WHERE `SR_NO`='$sr_no'";
+$sql  = "SELECT  `NAME` , `PRICE`, `IMAGE`,  `DISCRIPTION`, `CATEGORY`, `COLOR` FROM products WHERE `SR_NO`='$sr_no'";
     $result = $conn->query($sql);
 if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $img = $row['IMAGE']; ?>
+    while ($row = $result->fetch_assoc()) { ?>
+      
+      <div class="col-md-5 col-sm-5 col-xs-12">                              
+                  <div class="aa-product-view-slider">                                
+                    <div id="demo-1" class="simpleLens-gallery-container">
+                      <div class="simpleLens-container">
+                        <div class="simpleLens-big-image-container"><a data-lens-image="img/view-slider/large/polo-shirt-1.png" class="simpleLens-lens-image"><img src="admin/<?php echo $row['IMAGE']?>" class="simpleLens-big-image"></a></div>
+                      </div>
+                      <div class="simpleLens-thumbnails-container">
+                          <a data-big-image="img/view-slider/medium/polo-shirt-1.png" data-lens-image="img/view-slider/large/polo-shirt-1.png" class="simpleLens-thumbnail-wrapper" href="#">
+                            <img src="img/view-slider/thumbnail/polo-shirt-1.png">
+                          </a>                                    
+                          <a data-big-image="img/view-slider/medium/polo-shirt-3.png" data-lens-image="img/view-slider/large/polo-shirt-3.png" class="simpleLens-thumbnail-wrapper" href="#">
+                            <img src="img/view-slider/thumbnail/polo-shirt-3.png">
+                          </a>
+                          <a data-big-image="img/view-slider/medium/polo-shirt-4.png" data-lens-image="img/view-slider/large/polo-shirt-4.png" class="simpleLens-thumbnail-wrapper" href="#">
+                            <img src="img/view-slider/thumbnail/polo-shirt-4.png">
+                          </a>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
      <div class="col-md-7 col-sm-7 col-xs-12">
                   <div class="aa-product-view-content">
                     <h3><?php echo $row['NAME'];?></h3>
@@ -23,6 +43,9 @@ if ($result->num_rows > 0) {
                       <a href="#">XL</a>
                     </div>
                     <h4>Color</h4>
+
+                     
+
                     <div class="aa-color-tag">
                       <a href="#" class="aa-color-green"></a>
                       <a href="#" class="aa-color-yellow"></a>
@@ -46,12 +69,19 @@ if ($result->num_rows > 0) {
                       </p>
                     </div>
                     <div class="aa-prod-view-bottom">
-                      <a class="aa-add-to-cart-btn" href="#">Add To Cart</a>
+                      <a class="aa-add-to-cart-btn" href="cart.php">Add To Cart</a>
                       <a class="aa-add-to-cart-btn" href="#">Wishlist</a>
                       <a class="aa-add-to-cart-btn" href="#">Compare</a>
                     </div>
                   </div>
                 </div>
+                <div class="tab-content">
+                <div class="tab-pane fade in active" id="description">
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                   <p><?php echo $row['DISCRIPTION'];?></p>
+                </div>
+              </div>
+                
 
 <?php 
     }
